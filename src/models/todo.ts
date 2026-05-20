c8769ff7836cd2ad4da6ac5d21ceee775ece078b
Dozy <dotuan14747@gmail.com>
2026-05-20 09:33:34 +0300

    Created models and schema for SubTodo

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 
+// Todo
 export interface Todo {
   id: string;
   projectId: string;
@@ -7,6 +8,7 @@ export interface Todo {
   description: string;
   status: "pending" | "in_progress" | "completed";
   priority: "low" | "medium" | "high";
+  subTodos: SubTodo[];
   createdAt: string;
   updatedAt: string;
 }
@@ -33,4 +35,32 @@ export const todoUpdateSchema = {
     .enum(["low", "medium", "high"])
     .optional()
     .describe("New todo priority"),
+};
+
+// SubTodo
+export interface SubTodo {
+  id: string;
+  todoId: string;
+  title: string;
+  description: string;
+  status: "pending" | "in_progress" | "completed";
+  createdAt: string;
+  updatedAt: string;
+}
+
+export const subTodoInputSchema = {
+  todoId: z.string().describe("Parent todo Id"),
+  title: z.string().describe("Subtodo title"),
+  description: z.string().optional().describe("Subtodo description"),
+};
+
+export const subTodoUpdateSchema = {
+  todoId: z.string().describe("Parent todo Id"),
+  subTodoId: z.string().describe("Subtodo Id"),
+  title: z.string().optional().describe("New subtodo title"),
+  description: z.string().optional().describe("New subtodo description"),
+  status: z
+    .enum(["pending", "in_progress", "completed"])
+    .optional()
+    .describe("New todo status"),
 };
